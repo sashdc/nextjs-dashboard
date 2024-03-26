778adf0d576c6225b8a4d3b451ce70ab0af90807
sashdc <saharshdc@gmail.com>
2024-03-26 13:35:42 -0400

    added authentication and logging in and out

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,27 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
   id: z.string(),
@@ -64,21 +85,33 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
   }
 
-  export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+  export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+  ) {
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
    
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
+    }
+   
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
    
     try {
       await sql`
-          UPDATE invoices
-          SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-          WHERE id = ${id}
-        `;
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
     } catch (error) {
       return { message: 'Database Error: Failed to Update Invoice.' };
     }
@@ -88,7 +121,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
   }
 
   export async function deleteInvoice(id: string) {
-    throw new Error('Not Implemented');
     try {
       await sql`DELETE FROM invoices WHERE id = ${id}`;
       revalidatePath('/dashboard/invoices');
